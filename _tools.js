20d8ee4c94ae9cd45678d169dd6f4dca983c724c
elpbatista <elpbatista@gmail.com>
2018-10-09 16:05:54 -0300

    adding tools and data inspector

---
+++ _tools.js
@@ -0,0 +1,35 @@
+/*
+occurrences = {
+  key_0:[[amnt,value_0][amnt,value_1]...[amnt,value_n]],
+  key_1:[[amnt,value_0][amnt,value_1]...[amnt,value_n]],
+  key_n:[[amnt,value_0][amnt,value_1]...[amnt,value_n]]
+}
+filter = {
+  key_0:[value_0,value_1,value_n],
+  key_1:[value_0,value_1,value_n],
+  key_n:[value_0,value_1,value_n]
+}
+*/
+function get_occurrences(array, filter = {}) {
+  var occurrences = {};
+  for (var i = 0; i < array.length; i++) {
+    for (var key in array[i]) {
+      var value = array[i][key];
+      if (!(key in filter)) {
+        filter[key] = [];
+        occurrences[key] = [];
+      } else {
+        if (filter[key].indexOf(value) < 0) {
+          filter[key].push(value);
+          occurrences[key].push([0, value]);
+        }
+        occurrences[key][filter[key].indexOf(value)][0]++
+      }
+    }
+  }
+  return occurrences;
+}
+
+module.exports = {
+  get_occurrences: get_occurrences
+}
