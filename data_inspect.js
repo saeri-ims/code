918295904de02af991877eda14789ebe51c87da5
elpbatista <elpbatista@gmail.com>
2018-10-11 17:35:06 -0300

    CoL Service

--- data_inspect.js
+++ data_inspect.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var request = require('request');
 var _t = require('/home/pb/Work/code/_tools.js');
 
 var data_path = '/home/pb/Work/SAERI/BugsDatabase/biosecurity/data/'
@@ -9,4 +10,15 @@ var bugs = JSON.parse(fs.readFileSync(data_path + 'Bugs.json'));
 
 // }
 // fs.writeFileSync(data_path+'Bugs.json',JSON.stringify(bugs));
-fs.writeFileSync(data_path+'bugs_audit.json',JSON.stringify(_t.get_occurrences(bugs)));
+
+
+// fs.writeFileSync(data_path+'bugs_audit.json',JSON.stringify(_t.get_occurrences(bugs)));
+var search = 'Pediculus+humanus+capitis';
+var url = "http://webservice.catalogueoflife.org/col/webservice?format=json&name="+search;
+
+request(url, function (error, response, body) {
+  // console.log('error:', error);
+  // console.log('statusCode:', response && response.statusCode);
+  // console.log('body:', body);
+  console.log (JSON.parse(body));
+});
