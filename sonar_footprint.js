f40632d3bea703387d7184067f1eb17e4d459843
elpbatista <elpbatista@gmail.com>
2018-10-25 13:47:38 -0300

    adding sonar_footprint 01 02 03

--- sonar_footprint.js
+++ sonar_footprint.js
@@ -4,7 +4,7 @@ var turf = require('@turf/turf');
 var data_path = '/home/pb/Sites/data/';
 
 var bathymt = JSON.parse(fs.readFileSync(data_path + 'Bathymetry_4326.geojson'));
-var southbb = JSON.parse(fs.readFileSync(data_path + 'SouthBB_4326.geojson'));
+var southbb = JSON.parse(fs.readFileSync(data_path + 'SouthBB03_4326.geojson'));
 
 var intersects = turf.lineIntersect(southbb, bathymt);
 
@@ -21,8 +21,8 @@ turf.featureEach(bathymt, function(currentFeature, featureIndex) {
       circle = turf.circle(currentPoint.geometry.coordinates, Math.abs(currentFeature.properties.Depth * 0.004),{step:8});
       circle.properties.depth = currentFeature.properties.Depth;
       sonarFootprint.features.push(circle);
-      console.log(sonarFootprint);
+      // console.log(sonarFootprint);
     });
   };
 });
-fs.writeFileSync(data_path+'sonarFootprint.geojson',JSON.stringify(sonarFootprint));
+// fs.writeFileSync(data_path+'sonarFootprint03.geojson',JSON.stringify(sonarFootprint));
