42f81c33d2cf01989262686d80ae1c22d8288835
elpbatista <elpbatista@gmail.com>
2019-06-24 16:40:06 -0300

    fixing mma_gs

--- mma_gf.js
+++ mma_gf.js
@@ -10,15 +10,16 @@ var combined = JSON.parse(fs.readFileSync(data_path + 'mma_gf_' + analysis + '_c
 var all_R = JSON.parse(fs.readFileSync(data_path + 'mma_gf_' + analysis + '_all_R.json'));
 
 var images = [];
+var result = [];
 
 for (var i = 0; i < combined.length; i++) {
-  image = combined[i]['image'];
+  var image = combined[i]['image'];
   if (analysis == 'benthos') {
-    var spp_name = combined[i]['spp_name'];
-    var spp_nmbr = combined[i]['n_points_per_spp'];
+    spp_name = combined[i]['spp_name'];
+    spp_nmbr = combined[i]['n_points_per_spp'];
   } else if (analysis == 'species') {
-    var spp_name = combined[i]['spp'].split('-')[1];
-    var spp_nmbr = combined[i]['spp_total'];
+    spp_name = combined[i]['spp'].split('-')[1];
+    spp_nmbr = combined[i]['spp_total'];
   }
   if (!images.includes(image)) {
     var trans = {};
@@ -34,17 +35,19 @@ for (var i = 0; i < combined.length; i++) {
     trans['transect'] = combined[i]['transect'];
     trans[spp_name] = spp_nmbr;
     temp = JSON.parse(JSON.stringify(trans));
-    all_R.push(temp);
+    result.push(temp);
+    trans[spp_name] = '';
   } else {
-    all_R.map(function(e) {
-      if (e['image'] == image) {
-        e[spp_name] = spp_nmbr;
+    for (var j = 0; j < result.length; j++) {
+      if (result[j]['image'] === image) {
+        result[j][spp_name] = spp_nmbr;
+        break;
       }
-    });
+    }
   }
 }
-fs.writeFileSync(data_path + 'mma_gf_' + analysis + '_all_R_filled.json', JSON.stringify(all_R));
-conv.json2csv(all_R, function(err, csv) {
+fs.writeFileSync(data_path + 'mma_gf_' + analysis + '_all_R_filled.json', JSON.stringify(result));
+conv.json2csv(result, function(err, csv) {
   if (err) throw err;
   fs.writeFileSync(data_path + 'mma_gf_' + analysis + '_all_R.csv', csv);
 });
