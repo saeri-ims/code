4d2507de47c452884135aecd6e0d318929c937d3
elpbatista <elpbatista@gmail.com>
2019-07-25 15:39:42 -0300

    psg4ms.js

--- psg4ms.js
+++ psg4ms.js
@@ -1,41 +1,31 @@
-var fs = require('fs');
-var turf = require('@turf/turf');
-var cmd = require('node-cmd');
-var path = require('path');
+const fs = require('fs');
+const cmd = require('node-cmd');
+const path = require('path');
+const csv = require('csvtojson');
+
 var data_path = '/mnt/Data/Habitat_Surveys/data_hab_surveys/'
 var base_url = 'https://ims.saeri.org/psg4cm/';
 
-var data = JSON.parse(fs.readFileSync(data_path + 'manually/dsm4cm.geojson'));
-
-// fs.readdirSync(testFolder).forEach(file => {
-//   console.log(file);
-// });
-
-
-var count = 0;
-var groundValidationPoints = turf.featureCollection([]);
-turf.featureEach(data, function(currentFeature, featureIndex) {
-  count++; // counting gvps
-  var point = currentFeature.properties.Point_Number;
-  currentFeature.properties.Photo_N = base_url+point+'/N.jpg';
-  currentFeature.properties.Photo_E = base_url+point+'/E.jpg';
-  currentFeature.properties.Photo_S = base_url+point+'/S.jpg';
-  currentFeature.properties.Photo_W = base_url+point+'/W.jpg';
-  // Optimize
-  for (var property in currentFeature.properties) {
-    if (currentFeature.properties[property] == null || currentFeature.properties[property] == undefined || currentFeature.properties[property] == '') {
-      delete currentFeature.properties[property];
-    };
-    if (property == 'picture_ground' || property == 'picture_north' || property == 'picture_east' || property == 'picture_south' || property == 'picture_west') {
-      if (currentFeature.properties[property] != undefined) currentFeature.properties[property] = currentFeature.properties[property]['url'];
-    };
-  };
-
-  // Store
-  groundValidationPoints.features.push(currentFeature);
-
-});
-// console.log(groundValidationPoints);
-console.log(groundValidationPoints);
-
-fs.writeFileSync(data_path + 'Bran/' + (new Date()).toISOString().replace(/-/g, '').split('T')[0] + '_Soil4Coastal_Mapping.geojson', JSON.stringify(groundValidationPoints));
+csv()
+  .fromFile(data_path + 'manually/dsg4cm.csv')
+  .then((jsonObj) => {
+    var metadata_datasets = [];
+    for (var i = 0; i < jsonObj.length; i++) {
+      var waypoint = jsonObj[i]['waypoint'];
+      var photos_path = '/mnt/NAS/saeridata/2019_04_ImagesFromGroundValidation/';
+      try {
+        files = fs.readdirSync(photos_path + waypoint);
+      } catch (err) {
+        // An error occurred
+        console.error(err);
+      }
+      jsonObj[i]['Photo_Ground'] = base_url+waypoint+'/'+files[1];
+      jsonObj[i]['Photo_N'] = base_url+waypoint+'/'+files[2];
+      jsonObj[i]['Photo_E'] = base_url+waypoint+'/'+files[3];
+      jsonObj[i]['Photo_S'] = base_url+waypoint+'/'+files[4];
+      jsonObj[i]['Photo_W'] = base_url+waypoint+'/'+files[5];
+      console.log(jsonObj[i]);
+    }
+    fs.writeFileSync(data_path + 'forms/2019_04_ImagesFromGroundValidation.json', JSON.stringify(jsonObj));
+    console.log('Done!');
+  });
