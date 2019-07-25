4336b9f044417bf1daa9f68fe71ed9b57b1f6363
elpbatista <elpbatista@gmail.com>
2019-07-25 16:35:36 -0300

    fixing psg4ms.js

--- psg4ms.js
+++ psg4ms.js
@@ -19,11 +19,11 @@ csv()
         // An error occurred
         console.error(err);
       }
-      jsonObj[i]['picture_ground'] = base_url+waypoint+'/'+files[1];
-      jsonObj[i]['picture_north'] = base_url+waypoint+'/'+files[2];
-      jsonObj[i]['picture_east'] = base_url+waypoint+'/'+files[3];
-      jsonObj[i]['picture_south'] = base_url+waypoint+'/'+files[4];
-      jsonObj[i]['picture_west'] = base_url+waypoint+'/'+files[5];
+      jsonObj[i]['picture_g'] = base_url+waypoint+'/'+files[1];
+      jsonObj[i]['picture_n'] = base_url+waypoint+'/'+files[2];
+      jsonObj[i]['picture_e'] = base_url+waypoint+'/'+files[3];
+      jsonObj[i]['picture_s'] = base_url+waypoint+'/'+files[4];
+      jsonObj[i]['picture_w'] = base_url+waypoint+'/'+files[5];
       console.log(jsonObj[i]);
     }
     fs.writeFileSync(data_path + 'forms/2019_04_Grytviken_GroundValidationFromJamesWake.json', JSON.stringify(jsonObj));
